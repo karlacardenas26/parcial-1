e4c0acbc361d35681cd242264d42958f29de1f86
karlacardenas26 <60673907+karlacardenas26@users.noreply.github.com>
2020-02-06 16:49:45 -0600

    versión final
    Este que hice esta mal:c pero igual lo subi

---
+++ menor1.js
@@ -0,0 +1,10 @@
+var num1=math.floor(math.random()*20)+1;
+var num2=math.floor(math.random()*20)+1;
+var num3=math.floor(math.random()*20)+1;
+
+if (num1<num2);
+    if(nume1<num3);
+        else
+
+    else
+        if (num2<num3);
